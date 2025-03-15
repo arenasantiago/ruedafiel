b1c139a88bfc6df5f1553c755db0619c9ce2b0dc
arenasantiago <arenas4545a@gmail.com>
2025-03-15 12:27:59 -0500

    v. angular01

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { PrincipalPagesComponent } from './pages/principal/principal-pages.component';
+
+export const routes: Routes = [
+    {
+     path: 'principal',
+     component: PrincipalPagesComponent   
+    }
+];
---
+++ src/app/pages/principal/principal-pages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
+
+@Component({
+    templateUrl: './principal-pages.component.html',
+    imports:[RouterLink]
+})
+
+export class PrincipalPagesComponent{
+
+}
